6bd0ff9cc178a300ca65a51b896d2c75f976c313
Scott Hatcher <github@scotthatcher.com>
2018-12-07 11:40:53 -0800

    Optionally allow an allowClear param that allows the user to clear a selected value.

--- src/apChoice.ts
+++ src/apChoice.ts
@@ -1,72 +1,72 @@
 import { IndexedCache, ListItem } from 'angular-point';
 
 function createChoiceArray(options: string[] = []) {
-    return options.sort();
+  return options.sort();
 }
 
 export interface ITemplateOptions extends AngularFormly.ITemplateOptions {
-    lookupIdProperty: { (listItem: ListItem<any>): string } | string;
-    lookupValueProperty: { (listItem: ListItem<any>): string } | string;
-    options: Object[] | IndexedCache<ListItem<any>> | ng.IPromise<Object[] | IndexedCache<ListItem<any>>> | any;
+  lookupIdProperty: { (listItem: ListItem<any>): string } | string;
+  lookupValueProperty: { (listItem: ListItem<any>): string } | string;
+  options: Object[] | IndexedCache<ListItem<any>> | ng.IPromise<Object[] | IndexedCache<ListItem<any>>> | any;
 }
 
 export class APChoiceController {
-    static $inject = [];
-    key: string;
-    loading = true;
-    listItem: ListItem<any>;
-    multi: boolean;
-    options: string[];
-    placeholder: string | number;
-    to: ITemplateOptions;
+  static $inject = [];
+  allowClear = false;
+  key: string;
+  loading = true;
+  listItem: ListItem<any>;
+  multi: boolean;
+  options: string[];
+  placeholder: string | number;
+  to: ITemplateOptions;
 
-    $onInit() {
-        const $ctrl = this;
-        $ctrl.placeholder = $ctrl.to.placeholder || '';
-        const fieldDefinition = $ctrl.listItem.getFieldDefinition($ctrl.key);
+  $onInit() {
+    const $ctrl = this;
+    $ctrl.placeholder = $ctrl.to.placeholder || '';
+    $ctrl.allowClear = !!$ctrl.to.allowClear;
+    const fieldDefinition = $ctrl.listItem.getFieldDefinition($ctrl.key);
 
-        if ($ctrl.to.options) {
-            if ($ctrl.to.options.then) {
-                /** Options aren't resolved yet */
-                $ctrl.to.options.then(function (options: string[]) {
-                    $ctrl.options = createChoiceArray(options);
-                    $ctrl.loading = false;
-                });
-            } else {
-                /** Options passed through directly */
-                $ctrl.options = createChoiceArray($ctrl.to.options);
-                $ctrl.loading = false;
-            }
-        } else if (fieldDefinition.choices || fieldDefinition.Choices) {
-            /** Options available on field definition within model */
-            $ctrl.options = createChoiceArray(fieldDefinition.choices || fieldDefinition.Choices);
-            $ctrl.loading = false;
-        } else {
-            /** Last chance, get list definition from server and look for choices */
-            const model = $ctrl.listItem.getModel();
+    if ($ctrl.to.options) {
+      if ($ctrl.to.options.then) {
+        /** Options aren't resolved yet */
+        $ctrl.to.options.then(function(options: string[]) {
+          $ctrl.options = createChoiceArray(options);
+          $ctrl.loading = false;
+        });
+      } else {
+        /** Options passed through directly */
+        $ctrl.options = createChoiceArray($ctrl.to.options);
+        $ctrl.loading = false;
+      }
+    } else if (fieldDefinition.choices || fieldDefinition.Choices) {
+      /** Options available on field definition within model */
+      $ctrl.options = createChoiceArray(fieldDefinition.choices || fieldDefinition.Choices);
+      $ctrl.loading = false;
+    } else {
+      /** Last chance, get list definition from server and look for choices */
+      const model = $ctrl.listItem.getModel();
 
-            model.extendListMetadata()
-                .then(function () {
-                    if (fieldDefinition.choices || fieldDefinition.Choices) {
-                        $ctrl.options = createChoiceArray(fieldDefinition.choices || fieldDefinition.Choices);
-                        $ctrl.loading = false;
-                    }
-                })
+      model.extendListMetadata().then(function() {
+        if (fieldDefinition.choices || fieldDefinition.Choices) {
+          $ctrl.options = createChoiceArray(fieldDefinition.choices || fieldDefinition.Choices);
+          $ctrl.loading = false;
         }
-
+      });
     }
+  }
 }
 
-
 export const APFormlyChoiceComponent = {
-    bindings: {
-        key: '<',
-        listItem: '<',
-        multi: '<',
-        to: '=' //Template Options
-    },
-    controller: APChoiceController,
-    template: '' +
+  bindings: {
+    key: '<',
+    listItem: '<',
+    multi: '<',
+    to: '=', //Template Options
+  },
+  controller: APChoiceController,
+  template:
+    '' +
     `<div ng-if="!$ctrl.loading">
         <div ng-if="$ctrl.multi">
             <ui-select multiple ng-model="$ctrl.listItem[$ctrl.key]" ng-disabled="$ctrl.to.disabled">
@@ -76,10 +76,10 @@ export const APFormlyChoiceComponent = {
         </div>
         <div ng-if="!$ctrl.multi">
             <ui-select ng-model="$ctrl.listItem[$ctrl.key]" ng-disabled="$ctrl.to.disabled">
-                <ui-select-match placeholder="{{ $ctrl.placeholder }}">{{ $select.selected }}</ui-select-match>
+                <ui-select-match allow-clear="{{ $ctrl.allowClear }}" placeholder="{{ $ctrl.placeholder }}">{{ $select.selected }}</ui-select-match>
                 <ui-select-choices data-repeat="choice in $ctrl.options | filter:$select.search">{{ choice }}</ui-select-choices>
             </ui-select>
         </div>
     </div>
-    <span class="form-control" ng-if="$ctrl.loading">loading...</span>`
-}
+    <span class="form-control" ng-if="$ctrl.loading">loading...</span>`,
+};
--- src/apLookup.ts
+++ src/apLookup.ts
@@ -1,80 +1,86 @@
-import * as _ from 'lodash';
 import { IndexedCache, ListItem, Lookup } from 'angular-point';
+import * as _ from 'lodash';
 
-function createLookupArray(options, lookupIdProperty: { (listItem: ListItem<any>): string } | string, lookupValueProperty: { (listItem: ListItem<any>): string } | string): Lookup<any>[] {
-    let sortedLookupValues = [];
-    const sampleListItem = _.sample(options);
-    if (sampleListItem && sampleListItem.hasOwnProperty('lookupId')) {
-        /** Already valid lookup objects */
-        sortedLookupValues = _.sortBy(options, 'lookupValue');
-    } else if (sampleListItem) {
-        /** List items that need to ve converted into lookup objects */
-        sortedLookupValues = _.chain(options)
-            .map(function (listItem: ListItem<any>) {
-                return {
-                    // Default is to use title for lookupValue and id for lookupId but optionally can pass in the property
-                    // to use for either or a function to return the value
-                    lookupId: _.isFunction(lookupIdProperty) ? lookupIdProperty(listItem) : listItem[lookupIdProperty],
-                    // can be calculated with either a function or a property name
-                    lookupValue: _.isFunction(lookupValueProperty) ? lookupValueProperty(listItem) : listItem[lookupValueProperty]
-                };
-            })
-            .sortBy('lookupValue')
-            .value();
-    }
-    return sortedLookupValues;
+function createLookupArray(
+  options,
+  lookupIdProperty: { (listItem: ListItem<any>): string } | string,
+  lookupValueProperty: { (listItem: ListItem<any>): string } | string,
+): Lookup<any>[] {
+  let sortedLookupValues = [];
+  const sampleListItem = _.sample(options);
+  if (sampleListItem && sampleListItem.hasOwnProperty('lookupId')) {
+    /** Already valid lookup objects */
+    sortedLookupValues = _.sortBy(options, 'lookupValue');
+  } else if (sampleListItem) {
+    /** List items that need to ve converted into lookup objects */
+    sortedLookupValues = _.chain(options)
+      .map(function(listItem: ListItem<any>) {
+        return {
+          // Default is to use title for lookupValue and id for lookupId but optionally can pass in the property
+          // to use for either or a function to return the value
+          lookupId: _.isFunction(lookupIdProperty) ? lookupIdProperty(listItem) : listItem[lookupIdProperty],
+          // can be calculated with either a function or a property name
+          lookupValue: _.isFunction(lookupValueProperty)
+            ? lookupValueProperty(listItem)
+            : listItem[lookupValueProperty],
+        };
+      })
+      .sortBy('lookupValue')
+      .value();
+  }
+  return sortedLookupValues;
 }
 
 export interface ITemplateOptions {
-    // interface ITemplateOptions extends AngularFormly.ITemplateOptions {
-    lookupIdProperty: { (listItem: ListItem<any>): string } | string;
-    lookupValueProperty: { (listItem: ListItem<any>): string } | string;
-    options: angular.IPromise<Object[] | IndexedCache<ListItem<any>>> | Object[] | IndexedCache<ListItem<any>> | any;
-    placeholder?: string;
+  // interface ITemplateOptions extends AngularFormly.ITemplateOptions {
+  lookupIdProperty: { (listItem: ListItem<any>): string } | string;
+  lookupValueProperty: { (listItem: ListItem<any>): string } | string;
+  options: angular.IPromise<Object[] | IndexedCache<ListItem<any>>> | Object[] | IndexedCache<ListItem<any>> | any;
+  placeholder?: string;
 }
 
-
 export class APLookupController {
-    static $inject = [];
-    key: string;
-    listItem: Object;
-    loading = true;
-    multi: boolean;
-    options: Lookup<any>[];
-    placeholder: string | number;
-    to: ITemplateOptions;
-    $onInit() {
-        const $ctrl = this;
-        // The property to use as the lookupValue if we need to build a Lookup[]
-        const lookupIdProperty = $ctrl.to.lookupIdProperty || 'id';
-        const lookupValueProperty = $ctrl.to.lookupValueProperty || 'title';
+  static $inject = [];
+  allowClear = false;
+  key: string;
+  listItem: Object;
+  loading = true;
+  multi: boolean;
+  options: Lookup<any>[];
+  placeholder: string | number;
+  to: ITemplateOptions;
+  $onInit() {
+    const $ctrl = this;
+    // The property to use as the lookupValue if we need to build a Lookup[]
+    const lookupIdProperty = $ctrl.to.lookupIdProperty || 'id';
+    const lookupValueProperty = $ctrl.to.lookupValueProperty || 'title';
 
-        $ctrl.placeholder = $ctrl.to.placeholder || '';
+    $ctrl.placeholder = $ctrl.to.placeholder || '';
+    $ctrl.allowClear = !!$ctrl.to.allowClear;
 
-        if ($ctrl.to.options.then) {
-            /** Options aren't resolved yet */
-            $ctrl.to.options.then(function (options) {
-                $ctrl.options = createLookupArray(options, lookupIdProperty, lookupValueProperty);
-                $ctrl.loading = false;
-            });
-        } else {
-            $ctrl.options = createLookupArray($ctrl.to.options, lookupIdProperty, lookupValueProperty);
-            $ctrl.loading = false;
-        }
-
+    if ($ctrl.to.options.then) {
+      /** Options aren't resolved yet */
+      $ctrl.to.options.then(function(options) {
+        $ctrl.options = createLookupArray(options, lookupIdProperty, lookupValueProperty);
+        $ctrl.loading = false;
+      });
+    } else {
+      $ctrl.options = createLookupArray($ctrl.to.options, lookupIdProperty, lookupValueProperty);
+      $ctrl.loading = false;
     }
+  }
 }
 
 export const APFormlyLookupComponent = {
-    bindings: {
-        key: '<',
-        listItem: '<',
-        multi: '<',
-        to: '='
-
-    },
-    controller: APLookupController,
-    template: '' +
+  bindings: {
+    key: '<',
+    listItem: '<',
+    multi: '<',
+    to: '=',
+  },
+  controller: APLookupController,
+  template:
+    '' +
     `<div ng-if="!$ctrl.loading">
         <div ng-if="$ctrl.multi">
             <ui-select multiple ng-model="$ctrl.listItem[$ctrl.key]" ng-disabled="$ctrl.to.disabled">
@@ -85,11 +91,11 @@ export const APFormlyLookupComponent = {
         </div>
         <div ng-if="!$ctrl.multi">
             <ui-select ng-model="$ctrl.listItem[$ctrl.key]" ng-disabled="$ctrl.to.disabled">
-                <ui-select-match placeholder="{{ $ctrl.placeholder }}">{{ $select.selected.lookupValue }}</ui-select-match>
+                <ui-select-match allow-clear="{{ $ctrl.allowClear }}" placeholder="{{ $ctrl.placeholder }}">{{ $select.selected.lookupValue }}</ui-select-match>
                 <ui-select-choices data-repeat="lookup in $ctrl.options | filter:{lookupValue: $select.search}
                     track by lookup.lookupId">{{ lookup.lookupValue }}</ui-select-choices>
             </ui-select>
         </div>
     </div>
-    <span ng-if="$ctrl.loading">loading...</span>`
+    <span ng-if="$ctrl.loading">loading...</span>`,
 };
